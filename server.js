b8926f50a22102abf58c690a94ffef776c632fab
Balaji-02 <balajimohan2702@gmail.com>
2024-05-03 08:31:30 +0530

    Adding files

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' || req.url === '/index.html') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+      }
+    });
+  }
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
